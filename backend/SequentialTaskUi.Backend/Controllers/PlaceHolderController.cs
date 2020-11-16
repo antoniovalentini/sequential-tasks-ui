6e30447816e3fc5ddf3344b7c9ea6c58f22f1d89
antoniovalentini <antonio.valentini@live.it>
2020-11-16 21:52:06 +0100

    Add simple api endpoints

---
+++ backend/SequentialTaskUi.Backend/Controllers/PlaceHolderController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SequentialTaskUi.Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PlaceHolderController : ControllerBase
+    {
+        [HttpGet]
+        public PlaceHolder GetSuccess()
+        {
+            var rng = new Random();
+            var waitTime = rng.Next(500, 1500);
+            Thread.Sleep(waitTime);
+            return new PlaceHolder
+            {
+                Response = $"{nameof(GetSuccess)} has succeeded and returned in ~{waitTime}ms.",
+            };
+        }
+        
+        [HttpGet]
+        public PlaceHolder GetFailed()
+        {
+            var rng = new Random();
+            var waitTime = rng.Next(200, 700);
+            Thread.Sleep(waitTime);
+            return new PlaceHolder
+            {
+                Error = $"{nameof(GetFailed)} has encountered and error and returned in ~{waitTime}ms.",
+            };
+        }
+    }
+
+    public class PlaceHolder : GenericResponse
+    {
+        public string Response { get; set; }
+    }
+
+    public class GenericResponse
+    {
+        public string Error { get; set; }
+    }
+}
